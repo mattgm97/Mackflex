f8520c17c4c762283b0b947be2b6f15fcf657ba7
Cristian Maeztra <cristian@maeztra.com>
2023-04-27 20:33:49 -0300

    bugfix: condition old price main style null

--- react/components/Product/ProgressiveDiscount/index.tsx
+++ react/components/Product/ProgressiveDiscount/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { DescontoProgressivoProps } from "./types";
 import { useProductDispatch } from "vtex.product-context/ProductDispatchContext";
 import { getDefaultSeller } from "./helpers/seller";
@@ -6,107 +6,119 @@ import { useProduct } from "vtex.product-context";
 
 import "./global.css";
 
-const ProgressiveDiscount: StorefrontFunctionComponent<DescontoProgressivoProps> = ({ value = 1, discountText = "" }) => {
-        const dispatch = useProductDispatch();
-        const product = useProduct();
-        const descontoClusterHighlights = product?.product?.clusterHighlights.length;
-        const desconto = product?.product?.clusterHighlights;
+const ProgressiveDiscount: StorefrontFunctionComponent<
+  DescontoProgressivoProps
+> = ({ value = 1, discountText = "" }) => {
+  const dispatch = useProductDispatch();
+  const product = useProduct();
+  const descontoClusterHighlights = product?.product?.clusterHighlights.length;
+  const desconto = product?.product?.clusterHighlights;
 
-        const onChange = () => {
-            dispatch({ type: "SET_QUANTITY", args: { quantity: value } });
-        };
+  const onChange = () => {
+    dispatch({ type: "SET_QUANTITY", args: { quantity: value } });
+  };
 
-        // seta o valor do desconto
-        const productContextValue = useProduct();
+  // seta o valor do desconto
+  const productContextValue = useProduct();
 
-        var productQuantity: any = productContextValue?.selectedQuantity;
+  var productQuantity: any = productContextValue?.selectedQuantity;
 
-        // @ts-ignore
-        const seller: any = getDefaultSeller(
-            // @ts-ignore
-            productContextValue?.selectedItem?.sellers.map((sellers, Price) => {
-                return sellers.commertialOffer.Price;
-            })
-        );
+  // @ts-ignore
+  const seller: any = getDefaultSeller(
+    // @ts-ignore
+    productContextValue?.selectedItem?.sellers.map((sellers, Price) => {
+      return sellers.commertialOffer.Price;
+    })
+  );
 
-        function descontoCalc(discountRate:any) {
+  function descontoCalc(discountRate: any) {
+    const discount = seller - seller * discountRate;
 
-          const discount = (seller - seller * discountRate)
+    const newDesconto = discount.toFixed(2).toString().replace(".", ",");
 
-          const newDesconto = discount.toFixed(2).toString().replace(".", ",");
-        
+    let newPrice = document?.querySelector<HTMLElement>(
+      ".vtex-store-components-3-x-price_sellingPrice--PriceBox .valor-final"
+    )!;
+    let oldPriceMain = document?.querySelector<HTMLElement>(
+      ".vtex-store-components-3-x-price_sellingPrice--PriceBox span"
+    )!;
 
-          let newPrice = document?.querySelector<HTMLElement>(".vtex-store-components-3-x-price_sellingPrice--PriceBox .valor-final")!;
-          let oldPriceMain = document?.querySelector<HTMLElement>(".vtex-store-components-3-x-price_sellingPrice--PriceBox span")!
-          oldPriceMain.style.display = 'none'
-          if (!newPrice) {
-            newPrice = window?.document?.createElement("div");
-            newPrice?.classList.add("valor-final");
+    oldPriceMain.style.display = "none";
 
+    if (!newPrice) {
+      newPrice = window?.document?.createElement("div");
+      newPrice?.classList.add("valor-final");
 
-            const oldPrice = window?.document?.querySelector(
-                ".vtex-store-components-3-x-price_sellingPrice--PriceBox"
-            );
-         
-            oldPrice?.append(newPrice);
-          } else{
-            newPrice.style.display = 'block'
-          }
-          // @ts-ignore
-          return newPrice?.innerText = `R$ ${newDesconto}`;
-        }
+      const oldPrice = window?.document?.querySelector(
+        ".vtex-store-components-3-x-price_sellingPrice--PriceBox"
+      );
 
+      oldPrice?.append(newPrice);
+    } else {
+      newPrice.style.display = "block";
+    }
+    // @ts-ignore
+    return (newPrice?.innerText = `R$ ${newDesconto}`);
+  }
 
-        useEffect(() => {
+  useEffect(() => {
+    console.log("Passei");
+    if (productQuantity < 10) {
+      let oldPriceMain = document?.querySelector<HTMLElement>(
+        ".vtex-store-components-3-x-price_sellingPrice--PriceBox span"
+      )!;
 
-          if (productQuantity < 10) {
+      if (!oldPriceMain) {
+        return;
+      } else {
+        oldPriceMain.style.display = "block";
+      }
 
-            let oldPriceMain = document?.querySelector<HTMLElement>(".vtex-store-components-3-x-price_sellingPrice--PriceBox span")!
-            oldPriceMain.style.display = 'block';
-            let newPrice = document?.querySelector<HTMLElement>(".vtex-store-components-3-x-price_sellingPrice--PriceBox .valor-final")!;
+      let newPrice = document?.querySelector<HTMLElement>(
+        ".vtex-store-components-3-x-price_sellingPrice--PriceBox .valor-final"
+      )!;
 
-            if(newPrice){
-              newPrice.style.display = 'none'
-            }
-           
-          }
+      if (newPrice) {
+        newPrice.style.display = "none";
+      }
+    }
 
-          if (productQuantity >= 10 && productQuantity <= 19) {
-            descontoCalc(0.03);
-          }
+    if (productQuantity >= 10 && productQuantity <= 19) {
+      descontoCalc(0.03);
+    }
 
-          if (productQuantity >= 20 && productQuantity <= 29) {
-            descontoCalc(0.08);
-          }
+    if (productQuantity >= 20 && productQuantity <= 29) {
+      descontoCalc(0.08);
+    }
 
-          if (productQuantity >= 30 && productQuantity <= 39) {
-            descontoCalc(0.1);
-          }
+    if (productQuantity >= 30 && productQuantity <= 39) {
+      descontoCalc(0.1);
+    }
 
-          if (productQuantity >= 40 && productQuantity <= 49) {
-            descontoCalc(0.15);
-          }
-          if (productQuantity >= 50) {
-            descontoCalc(0.17);
-          }
+    if (productQuantity >= 40 && productQuantity <= 49) {
+      descontoCalc(0.15);
+    }
+    if (productQuantity >= 50) {
+      descontoCalc(0.17);
+    }
+  }, [productQuantity, productContextValue?.selectedItem]);
 
-       
-        }, [ productQuantity, productContextValue?.selectedItem]);
-
-        return (
-          <div className="progressive-discount-container">
-            {descontoClusterHighlights != 0 && desconto?.[0].id == '138' ? (
-                <label  onClick={onChange}>
-                  <span className="progressive-discount-number">{value}{value >= 50 ? "+" : ""}</span>
-                  <span className="progressive-discount-units">Unidades</span>
-                  <span className="progressive-discount-discounts">
-                      {discountText}% off
-                  </span>
-                </label>
-              ): null  
-            }
-          </div>
-        );
+  return (
+    <div className="progressive-discount-container">
+      {descontoClusterHighlights != 0 && desconto?.[0].id == "138" ? (
+        <label onClick={onChange}>
+          <span className="progressive-discount-number">
+            {value}
+            {value >= 50 ? "+" : ""}
+          </span>
+          <span className="progressive-discount-units">Unidades</span>
+          <span className="progressive-discount-discounts">
+            {discountText}% off
+          </span>
+        </label>
+      ) : null}
+    </div>
+  );
 };
 
 export default ProgressiveDiscount;
